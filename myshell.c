4a2c36286e498e6cb3aa88fec124a93e85c0a281
mufeng964497595 <964497595@qq.com>
2018-05-04 19:14:33 +0800

    增加颜色显示，删除help，改为用command -v来判断命令是否存在

--- myshell.c
+++ myshell.c
@@ -16,6 +16,7 @@ const char* COMMAND_EXIT = "exit";
 const char* COMMAND_HELP = "help";
 const char* COMMAND_CD = "cd";
 
+// 内置的状态码
 enum {
 	RESULT_NORMAL,
 	ERROR_FORK,
@@ -30,7 +31,7 @@ enum {
 
 char username[BUF_SZ];
 char hostname[BUF_SZ];
-char outputFile[BUF_SZ];
+char outputFile[BUF_SZ]; // isCommandExist函数中的文件输出路径，用于检验命令是否存在
 char curPath[BUF_SZ];
 char commands[BUF_SZ][BUF_SZ];
 
@@ -40,7 +41,6 @@ void getHostname();
 int getCurWorkDir();
 int splitCommands(char command[BUF_SZ]);
 int callExit();
-int callHelp();
 int callCommand(int commandNum);
 int callCd(int commandNum);
 
@@ -49,17 +49,20 @@ int main() {
 
 	getUsername();
 	getHostname();
-
 	int result = getCurWorkDir();
 	if (ERROR_SYSTEM == result) {
-		fprintf(stderr, "Error: System error while getting current work directory.\n");
+		fprintf(stderr, "\e[31;1mError: System error while getting current work directory.\n\e[0m");
 		exit(ERROR_SYSTEM);
 	}
+
+	/* 更新outputFile的值 */
 	strcpy(outputFile, curPath);
 	strcat(outputFile, "/command.res");
 
-	while (1) {
-		printf("%s@%s:%s$ ", username, hostname,curPath);
+	/* 启动myshell */
+	while (TRUE) {
+		printf("\e[32;1m%s@%s:%s\e[0m$ ", username, hostname,curPath); // 显示为绿色
+		/* 获取用户输入的命令 */
 		fgets(argv, BUF_SZ, stdin);
 		int len = strlen(argv);
 		if (len != BUF_SZ) {
@@ -74,28 +77,25 @@ int main() {
 				if (ERROR_EXIT == result) {
 					exit(-1);
 				}
-			} else if (strcmp(commands[0], COMMAND_HELP) == 0) {
-				result = callHelp();
-				if (ERROR_FORK == result) {
-					fprintf(stderr, "Error: Fork error in line %d.\n", __LINE__);
-					exit(ERROR_FORK);
-				}
 			} else if (strcmp(commands[0], COMMAND_CD) == 0) {
 				result = callCd(commandNum);
 				switch (result) {
 					case ERROR_MISS_PARAMETER:
-						fprintf(stderr, "Error: Miss parameter while using command \"%s\".\n",COMMAND_CD);
+						fprintf(stderr, "\e[31;1mError: Miss parameter while using command \"%s\".\n\e[0m"
+							, COMMAND_CD);
 						break;
 					case ERROR_WRONG_PARAMETER:
-						fprintf(stderr, "Error: No such path \"%s\".\n", commands[1]);
+						fprintf(stderr, "\e[31;1mError: No such path \"%s\".\n\e[0m", commands[1]);
 						break;
 					case ERROR_TOO_MANY_PARAMETER:
-						fprintf(stderr, "Error: Too many parameters while using command \"%s\".\n", COMMAND_CD);
+						fprintf(stderr, "\e[31;1mError: Too many parameters while using command \"%s\".\n\e[0m"
+							, COMMAND_CD);
 						break;
 					case RESULT_NORMAL:
 						result = getCurWorkDir();
 						if (ERROR_SYSTEM == result) {
-							fprintf(stderr, "Error: System error while getting current work directory.\n");
+							fprintf(stderr
+								, "\e[31;1mError: System error while getting current work directory.\n\e[0m");
 							exit(ERROR_SYSTEM);
 						} else {
 							break;
@@ -105,10 +105,10 @@ int main() {
 				result = callCommand(commandNum);
 				switch (result) {
 					case ERROR_FORK:
-						fprintf(stderr, "Error: Fork error in line %d.\n", __LINE__);
+						fprintf(stderr, "\e[31;1mError: Fork error in line %d.\n\e[0m", __LINE__);
 						exit(ERROR_FORK);
 					case ERROR_COMMAND:
-						fprintf(stderr, "Error: Command not exist in myshell.\n");
+						fprintf(stderr, "\e[31;1mError: Command not exist in myshell.\n\e[0m");
 						break;
 				}
 			}
@@ -126,13 +126,16 @@ int isCommandExist(const char* command) {
 		result = FALSE;
 	} else if (pid == 0) {
 		freopen(outputFile, "w", stdout);
-		execlp("which", "which", command, NULL);
+		char tmp[BUF_SZ];
+		sprintf(tmp, "command -v %s", command);
+		system(tmp);
+	//	execlp("which", "which", command, NULL);
 		exit(1);
 	} else {
 		waitpid(pid, NULL, 0);
 
 		FILE* fp = fopen(outputFile, "r");
-		if (fp == NULL || fgetc(fp) == EOF) {
+		if (fp == NULL || fgetc(fp) == EOF) { // 文件不存在或者文件中无数据，意味着命令不存在
 			result = FALSE;
 		}
 		fclose(fp);
@@ -157,7 +160,7 @@ int getCurWorkDir() {
 	else return RESULT_NORMAL;
 }
 
-int splitCommands(char command[BUF_SZ]) {
+int splitCommands(char command[BUF_SZ]) { // 以空格分割命令， 返回分割得到的字符串个数
 	int num = 0;
 	int i, j;
 	int len = strlen(command);
@@ -181,15 +184,11 @@ int splitCommands(char command[BUF_SZ]) {
 	return num;
 }
 
-int callExit() {
+int callExit() { // 发送terminal信号退出进程
 	pid_t pid = getpid();
 	if (kill(pid, SIGTERM) == -1) 
 		return ERROR_EXIT;
 	else return RESULT_NORMAL;
-}
-
-int callHelp() {
-	return RESULT_NORMAL;
 }
 
 int callCommand(int commandNum) {
@@ -199,23 +198,22 @@ int callCommand(int commandNum) {
 
 	int result = RESULT_NORMAL;
 	pid_t pid = vfork();
-
 	if (pid == -1) {
 		result = ERROR_FORK;
-	} else if (pid == 0) {
+	} else if (pid == 0) { // 执行命令
 		char* comm[BUF_SZ];
 		for (int i=0; i<commandNum; ++i)
 			comm[i] = commands[i];
 		comm[commandNum] = NULL;
 		execvp(comm[0], comm);
-		exit(errno);
+		exit(errno); // 执行出错，返回errno
 	} else {
 		int status;
 		waitpid(pid, &status, 0);
-		int err = WEXITSTATUS(status);
+		int err = WEXITSTATUS(status); // 读取子进程的返回码
 
-		if (err) {
-			printf("Error: %s\n", strerror(err));
+		if (err) { // 返回码不为0，意味着子进程执行出错，用红色字体打印出错信息
+			printf("\e[31;1mError: %s\n\e[0m", strerror(err));
 		}
 	}
 
